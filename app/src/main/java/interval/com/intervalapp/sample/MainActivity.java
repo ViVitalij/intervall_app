5315a01330b22be34400c715ea33beaeb2f38c92
unknown <doris.bednarska@gmail.com>
2017-05-29 20:47:52 +0200

    Added new login layout

--- app/src/main/java/interval/com/intervalapp/sample/MainActivity.java
+++ app/src/main/java/interval/com/intervalapp/sample/MainActivity.java
@@ -1,16 +1,32 @@
 package interval.com.intervalapp.sample;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import interval.com.intervalapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+            String fontPath="fonts/Pacifico.ttf";
+        TextView aboutus=(TextView)findViewById(R.id.textView2);
+        Typeface type= Typeface.createFromAsset(getAssets(), fontPath);
+        aboutus.setTypeface(type);
     }
 
+//    Typeface fonts = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
+//    TextView txtSampleTxt = (TextView) findViewById(R.id.textView2);
+//    txtSampleTxt.setTypeface(fonts);
+
+
+
 }
+
