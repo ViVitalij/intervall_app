b6ac5c1c2207db338ebba18573f6d5c4f2a3c46b
unknown <doris.bednarska@gmail.com>
2017-05-30 20:59:56 +0200

    first commit

--- app/src/main/java/interval/com/intervalapp/sample/LoginLayout.java
+++ app/src/main/java/interval/com/intervalapp/sample/LoginLayout.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import interval.com.intervalapp.R;
 
 public class LoginLayout extends AppCompatActivity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        ButterKnife.bind(this);
 
         String fontPath = "fonts/Pacifico.ttf";
         TextView aboutus = (TextView) findViewById(R.id.textView2);
         Typeface type = Typeface.createFromAsset(getAssets(), fontPath);
         aboutus.setTypeface(type);
+    }
 
-//        View decorView = getWindow().getDecorView();
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//        decorView.setSystemUiVisibility(uiOptions);
-//        ActionBar actionBar = getActionBar();
-//        actionBar.hide();
+   @OnClick(R.id.new_account)
+    void newAccountClicked(){
+
     }
 }
 
