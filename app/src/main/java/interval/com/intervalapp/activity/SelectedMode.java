805786542896bff00c1ee04627ab27a6adad4c32
Vitalij <kontrabasista@wp.pl>
2017-06-03 10:11:09 +0200

    refactor run/selected mode activities

--- app/src/main/java/interval/com/intervalapp/activity/SelectedMode.java
+++ app/src/main/java/interval/com/intervalapp/activity/SelectedMode.java
@@ -1,13 +1,29 @@
 package interval.com.intervalapp.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.dd.morphingbutton.MorphingButton;
+import com.dd.morphingbutton.impl.LinearProgressButton;
 
 import interval.com.intervalapp.R;
+import interval.com.intervalapp.chronometer.Chronometer;
+import interval.com.intervalapp.utils.ProgressGenerator;
 
+public class SelectedMode extends BaseActivity {
+    private int mMorphCounter1 = 1;
+    Chronometer focus;
+    LinearProgressButton start;
+    boolean isStart;
 
-public class SelectedMode extends AppCompatActivity {
+    public static void startThisActivity(@NonNull Context context) {
+        context.startActivity(new Intent(context, SelectedMode.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +31,83 @@ public class SelectedMode extends AppCompatActivity {
         setContentView(R.layout.selected_mode_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final Chronometer mChronometer = (Chronometer) findViewById(R.id.chronometer);
+
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            public void onChronometerTick(Chronometer chronometer) {
+                if (chronometer.getText().toString().equalsIgnoreCase("00:05:0")) {
+                    chronometer.stop();
+                    Toast.makeText(getBaseContext(), "Reached the end.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        final LinearProgressButton startButton = (LinearProgressButton) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onMorphButton1Clicked(startButton);
+                mChronometer.start();
+            }
+        });
+        morphToSquare(startButton, 0);
+    }
+
+
+    private void onMorphButton1Clicked(final LinearProgressButton btnMorph) {
+        if (mMorphCounter1 == 0) {
+            mMorphCounter1++;
+            morphToSquare(btnMorph, integer(R.integer.mb_animation));
+        } else if (mMorphCounter1 == 1) {
+            mMorphCounter1 = 0;
+            simulateProgress1(btnMorph);
+        }
+    }
+
+
+    private void morphToSquare(final MorphingButton btnMorph, int duration) {
+        MorphingButton.Params circle = MorphingButton.Params.create()
+                .duration(duration)
+                .cornerRadius(dimen(R.dimen.mb_height_56))
+                .width(dimen(R.dimen.mb_height_56))
+                .height(dimen(R.dimen.mb_height_56))
+                .color(color(R.color.start))
+                .colorPressed(color(R.color.start))
+                .text(getString(R.string.mb_button));
+        btnMorph.morph(circle);
+    }
+
+    private void morphToSuccess(final MorphingButton btnMorph) {
+        MorphingButton.Params circle = MorphingButton.Params.create()
+                .duration(integer(R.integer.mb_animation))
+                .cornerRadius(dimen(R.dimen.mb_height_56))
+                .width(dimen(R.dimen.mb_height_56))
+                .height(dimen(R.dimen.mb_height_56))
+                .color(color(R.color.colorAccent))
+                .colorPressed(color(R.color.colorAccent))
+                .icon(R.drawable.ic_done);
+        btnMorph.morph(circle);
+    }
+
+    private void simulateProgress1(@NonNull final LinearProgressButton button) {
+        int progressColor = color(R.color.colorAccent);
+        int color = color(R.color.mb_gray);
+        int progressCornerRadius = dimen(R.dimen.mb_corner_radius_4);
+        int width = dimen(R.dimen.mb_width_200);
+        int height = dimen(R.dimen.mb_height_8);
+        int duration = integer(R.integer.mb_animation);
+
+        ProgressGenerator generator = new ProgressGenerator(new ProgressGenerator.OnCompleteListener() {
+            @Override
+            public void onComplete() {
+                morphToSuccess(button);
+                button.unblockTouch();
+            }
+        });
+        button.blockTouch(); // prevent user from clicking while button is in progress
+        button.morphToProgress(color, progressColor, progressCornerRadius, width, height, duration);
+        generator.start(button);
     }
 }
