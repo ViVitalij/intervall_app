c03203425dabac985cf72a2805202354e426fc1a
Vitalij <kontrabasista@wp.pl>
2017-06-03 16:11:03 +0200

    added first version of firebase authentication

--- app/src/main/java/interval/com/intervalapp/activity/LoginActivity.java
+++ app/src/main/java/interval/com/intervalapp/activity/LoginActivity.java
@@ -3,18 +3,34 @@ package interval.com.intervalapp.activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import interval.com.intervalapp.R;
+import interval.com.intervalapp.helper.ResetPasswordHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
     @BindView(R.id.appName_textView)
     TextView aboutUsTextView;
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnLogin;
+    private TextView btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,17 @@ public class LoginActivity extends AppCompatActivity {
         String fontPath = "fonts/Pacifico.ttf";
         Typeface type = Typeface.createFromAsset(getAssets(), fontPath);
         aboutUsTextView.setTypeface(type);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, ResetPasswordHelper.class));
+            finish();
+        }
+        //TODO butterknife
+        inputEmail = (EditText) findViewById(R.id.loginEmail);
+        inputPassword = (EditText) findViewById(R.id.loginPassword);
+        btnSignup = (TextView) findViewById(R.id.new_account);
+        btnLogin = (Button) findViewById(R.id.login_button);
+        auth = FirebaseAuth.getInstance();
     }
 
     @OnClick(R.id.new_account)
@@ -34,7 +61,33 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.login_button)
     protected void loginButtonClicked() {
-        startActivity(new Intent(this, ModeActivity.class));
+        String email = inputEmail.getText().toString();
+        final String password = inputPassword.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //TODO getApplicationContex nawiast!
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            if (password.length() < 6) {
+                                inputPassword.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, ModeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
     }
 }
-
--- app/src/main/java/interval/com/intervalapp/activity/NewAccountActivity.java
+++ app/src/main/java/interval/com/intervalapp/activity/NewAccountActivity.java
@@ -1,15 +1,84 @@
 package interval.com.intervalapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import interval.com.intervalapp.R;
 
 public class NewAccountActivity extends AppCompatActivity {
 
+    private EditText inputEmail, inputPassword;
+    private Button btnSignUp;
+    private FirebaseAuth auth;
+    private TextView btnResetPassword;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_account_activity);
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+        btnSignUp = (Button) findViewById(R.id.createAccountButton);
+        inputEmail = (EditText) findViewById(R.id.accountEmail);
+        inputPassword = (EditText) findViewById(R.id.accountPassword);
+        btnResetPassword = (TextView) findViewById(R.id.forget_password_textView);
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NewAccountActivity.this, ResetPasswordActivity.class));
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(NewAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(NewAccountActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(NewAccountActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(NewAccountActivity.this, LoginActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
