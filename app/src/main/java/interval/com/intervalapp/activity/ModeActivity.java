397883355192aa54cf1a9891c010bb694559eac0
ViVitalij <kontrabasista@wp.pl>
2017-06-09 23:55:09 +0200

    asking about permission without doubt

--- app/src/main/java/interval/com/intervalapp/activity/ModeActivity.java
+++ app/src/main/java/interval/com/intervalapp/activity/ModeActivity.java
@@ -1,5 +1,24 @@
 package interval.com.intervalapp.activity;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+
+
 import android.Manifest;
 import android.content.ClipData;
 import android.content.Intent;
@@ -46,7 +65,11 @@ import io.realm.RealmResults;
  * Created by m.losK on 19.05.2017.
  */
 
-public class ModeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ModeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private View view;
+
 
     private static final int READ_EXTERNAL_STORAGE_REQUEST = 234;
     @BindView(R.id.drawer_layout)
@@ -69,11 +92,112 @@ public class ModeActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.mode_activity);
         ButterKnife.bind(this);
 
+        Button check_permission = (Button) findViewById(R.id.check_permission);
+        Button request_permission = (Button) findViewById(R.id.request_permission);
+        check_permission.setOnClickListener(this);
+        request_permission.setOnClickListener(this);
+
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemIconTintList(null);
 
         initList();
     }
+
+    @Override
+    public void onClick(View v) {
+
+        view = v;
+
+        int id = v.getId();
+        switch (id) {
+            case R.id.check_permission:
+                if (checkPermission()) {
+
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+
+                } else {
+
+                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.request_permission:
+                if (!checkPermission()) {
+
+                    requestPermission();
+
+                } else {
+
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (locationAccepted && cameraAccepted)
+                        Snackbar.make(view, "Permission Granted, Now you can access write and read from external storage.", Snackbar.LENGTH_LONG).show();
+                    else {
+
+                        Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(ModeActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+
 
     @OnClick(R.id.fab)
     protected void buttonClicked(View view) {
@@ -89,12 +213,7 @@ public class ModeActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_musicPicker) {
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
-                , READ_EXTERNAL_STORAGE_REQUEST);
-                Log.i("TAG", "PERMISSION NEEDED");
-                return false;
-            }
+
             Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
             chooseFile.setType("audio/*");
             chooseFile.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
