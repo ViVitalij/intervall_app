7953483bda05a2ea2b8141e6783d3a26a182444c
ViVitalij <kontrabasista@wp.pl>
2017-06-19 16:57:54 +0200

    mipmaps and drawables separation

--- app/src/main/java/interval/com/intervalapp/activity/SelectedMode.java
+++ app/src/main/java/interval/com/intervalapp/activity/SelectedMode.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,12 +15,18 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import interval.com.intervalapp.R;
 
+import static interval.com.intervalapp.R.id.circular_progress_bar;
+
 public class SelectedMode extends AppCompatActivity {
 
     @BindView(R.id.chronometer)
     protected Chronometer chronometer;
+
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
+
+    @BindView(circular_progress_bar)
+    protected CircularProgressBar circularProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +34,31 @@ public class SelectedMode extends AppCompatActivity {
         setContentView(R.layout.selected_mode_activity);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        setCircularProgressBar();
     }
 
-    @OnClick(R.id.stop_button)
-    void stopButtonClicked() {
-        showAlertDialog();
-    }
-
-    @OnClick(R.id.pause_button)
-    void pauseButtonClicked() {
+    private void setCircularProgressBar() {
+        circularProgressBar.setColor(ContextCompat.getColor(this, R.color.low_intensity));
+        circularProgressBar.setBackgroundColor(ContextCompat.getColor(this, R.color.medium_intensity));
+        circularProgressBar.setProgressBarWidth(getResources().getDimension(R.dimen.progress_bar_width));
+        circularProgressBar.setBackgroundProgressBarWidth(getResources().getDimension(R.dimen.progress_bar_background_width));
     }
 
     @OnClick(R.id.start_button)
-    void startButtonClicked() {
+    protected void startButtonClicked() {
         chronometer.setBase(SystemClock.elapsedRealtime());
         chronometer.start();
+        circularProgressBar.setProgressWithAnimation(100, 6000);
+    }
+
+    @OnClick(R.id.pause_button)
+    protected void pauseButtonClicked() {
+        circularProgressBar.stopAnimation();
+    }
+
+    @OnClick(R.id.stop_button)
+    protected void stopButtonClicked() {
+        showAlertDialog();
     }
 
     private void showAlertDialog() {
@@ -52,9 +69,12 @@ public class SelectedMode extends AppCompatActivity {
                         "Yes",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                circularProgressBar.stopAnimation();
+                                circularProgressBar.setProgress(0);
                                 Intent intent = new Intent(getApplicationContext(), SummaryActivity.class);
                                 startActivity(intent);
-                                dialog.cancel();
+                                finish();
                             }
                         })
                 .setNegativeButton(
